c0af295dfcf95a0ff84becb4f5155c722c282340
june lee <melodysdreamj@gmail.com>
2024-05-23 23:12:08 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/widget_binding_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForWidgetBindingLego(BuildContext context) async {}
